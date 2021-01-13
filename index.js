e6908cdf4aee0fa335acf97b094555112d9fb3a2
Mateus <mateus4k@protonmail.ch>
2021-01-13 16:03:19 -0300

    :rocket: convert main function into async

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const readline = require('readline')
 const fs = require('fs')
-const dns = require('dns')
+const { lookup } = require('dns')
+const { promisify } = require('util')
 
 const print = (...args) => console.log(...args)
 
-if (process.argv.length <= 2) {
-  print('Usage: npm run init <domain>')
-  process.exit()
+const checkArgs = (args) => args.length > 2
+
+const asyncLookup = promisify(lookup)
+
+const readWords = async (customWordlist = null) => {
+  return fs
+    .readFileSync(customWordlist || 'wordlist.txt')
+    .toString('utf-8')
+    .replace(/\r\n/g, '\n').split('\n')
 }
 
-const domain = process.argv[2]
+const lookupDns = async (url) => {
+  const result = await asyncLookup(url)
+  return result.address ? result.address : null
+}
 
-const createdInterface = readline.createInterface({
-  input: fs.createReadStream('wordlist.txt')
-})
+(async () => {
+  const [...args] = process.argv
 
-const lookupDns = (host) => {
-  const url = `${host}.${domain}`
-  dns.lookup(url, (_err, address) => {
-    if (address) {
-      print(`${url} | ${address}`)
-    }
+  if (!checkArgs(args)) {
+    print('Usage: npm run init <domain>')
+    process.exit()
+  }
+
+  const domain = args[2]
+  const customWordlist = args[3]
+
+  const words = await readWords(customWordlist)
+
+  const getHostsDns = () => words.map(async host => {
+    const url = `${host}.${domain}`
+
+    try {
+      const dns = await lookupDns(url)
+
+      if (dns) {
+        print(`${url} | ${dns}`)
+      }
+    } catch (error) {}
   })
-}
 
-createdInterface.on('line', lookupDns)
+  await Promise.all(getHostsDns())
+})()
