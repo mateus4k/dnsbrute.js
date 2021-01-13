ab9a23c661bfadc606f5a71cb908d7f09e893a0f
Mateus <mateus4k@protonmail.ch>
2021-01-13 14:58:00 -0300

    :rocket: Add initial feature

---
+++ index.js
@@ -0,0 +1,27 @@
+const readline = require('readline')
+const fs = require('fs')
+const dns = require('dns')
+
+const print = (...args) => console.log(...args)
+
+if (process.argv.length <= 2) {
+  print('Usage: npm run init <domain>')
+  process.exit(-1)
+}
+
+const domain = process.argv[2]
+
+const createdInterface = readline.createInterface({
+  input: fs.createReadStream('wordlist.txt')
+})
+
+const lookupDns = (host) => {
+  const url = `${host}.${domain}`
+  dns.lookup(url, (_err, address) => {
+    if (address) {
+      print(`${url} | ${address}`)
+    }
+  })
+}
+
+createdInterface.on('line', lookupDns)
